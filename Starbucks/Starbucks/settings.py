37ebe23b3ed223e90424e1ed88a4a84f800d8dfb
Keshav Purohit <122084995+Kpurohit007@users.noreply.github.com>
2024-11-15 17:32:08 +0530

    Update settings.py

--- Starbucks/Starbucks/settings.py
+++ Starbucks/Starbucks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fmou68qx+(b4%yn*h5s5g%7hmlw32)d!+)lizz5p*#e(^k%)7%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://starbucks-shop-production.up.railway.app']
 
 
 # Application definition
